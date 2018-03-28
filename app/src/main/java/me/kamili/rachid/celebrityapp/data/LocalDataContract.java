24295cf7165b24dd3a6560a498f3a64a71ec525d
rachidKamili <rachidkamili.pro@gmail.com>
2018-03-28 17:21:14 -0400

    Create the database contract

---
+++ app/src/main/java/me/kamili/rachid/celebrityapp/data/LocalDataContract.java
@@ -0,0 +1,32 @@
+package me.kamili.rachid.celebrityapp.data;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by Admin on 3/28/2018.
+ */
+
+public class LocalDataContract {
+
+    public static final String NAME = "Celebrity.db";
+    public static final int VERSION = 1;
+
+    public static final String CREATE_CELEBRITY_TABLE
+            = "CREATE TABLE " + Celebrity.TABLE + "(" +
+            Celebrity.FIRST_NAME + " TEXT, " +
+            Celebrity.LAST_NAME + " TEXT, " +
+            Celebrity.OCCUPATION + " TEXT, " +
+            Celebrity.ISFAVORITE + " INTEGER DEFAULT 0)";
+
+    public static final String GET_ALL = "SELECT * FROM " + Celebrity.TABLE;
+
+    public static class Celebrity implements BaseColumns {
+
+        public static final String TABLE = "Celebrity";
+        public static final String FIRST_NAME = "firstName";
+        public static final String LAST_NAME = "lastName";
+        public static final String OCCUPATION = "occupation";
+        public static final String ISFAVORITE = "isFavorite";
+    }
+
+}
